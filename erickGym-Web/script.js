afc757726f57f39afe2617c3c3cce2a8db8641be
Gisele <you@example.com>
2024-03-22 13:03:58 -0300

    alteracoes modificar/remover

--- erickGym-Web/script.js
+++ erickGym-Web/script.js
@@ -71,7 +71,7 @@ async function apagarExercicio(id) {
                 const nome = exercicio.nome
                 const descricao = exercicio.descricao
 
-                const dados = {nome, descricao }
+                const dados = { nome, descricao }
 
                 const config = {
                     method: 'DELETE',
@@ -117,79 +117,81 @@ async function apagarExercicio(id) {
                 alert('Erro ao deletar exercicio!')
             }
         }
+    }
+}
 
 
-        async function modificarExercicio(id) {
-            const response = await fetch(API_URL)
-            if (response.status === 200) {
-                const exercicios = await response.json()
-                for (let exercicio of exercicios) {
-                    if (exercicio.id === id) {
-                        cx_nome.value = exercicio.nome
-                        cx_descricao.value = exercicio.descricao
+async function modificarExercicio(id) {
+    const response = await fetch(API_URL)
+    if (response.status === 200) {
+        const exercicios = await response.json()
+        for (let exercicio of exercicios) {
+            if (exercicio.id === id) {
+                cx_nome.value = exercicio.nome
+                cx_descricao.value = exercicio.descricao
 
-                        const nome = exercicio.nome
-                        const descricao = exercicio.descricao
+                const nome = exercicio.nome
+                const descricao = exercicio.descricao
 
-                        const dados = { nome, descricao }
+                const dados = { nome, descricao }
 
-                        const config = {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json'
-                            },
-                            body: JSON.stringify(dados)
-                        }
-
-                        const response = await fetch(API_URL, config)
-
-                        if (response.status === 201) {
-                            const exercicio = await response.json()
-                            adicionarItemNaLista(exercicio)
-                            alert('Exercicio modificado com sucesso!')
-                        }
-                        else {
-                            alert('Erro ao modificar exercicio!')
-                        }
-                    }
+                const config = {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(dados)
                 }
 
+                const response = await fetch(API_URL, config)
 
-
-
+                if (response.status === 201) {
+                    const exercicio = await response.json()
+                    adicionarItemNaLista(exercicio)
+                    alert('Exercicio modificado com sucesso!')
+                }
+                else {
+                    alert('Erro ao modificar exercicio!')
+                }
             }
         }
 
 
 
 
-
-        function adicionarItemNaLista(exercicio) {
-            const item_nome = document.createElement('td')
-            const item_descricao = document.createElement('td')
-            const modificar = document.createElement('td')
-            const deletar = document.createElement('td')
-            const linha = document.createElement('tr')
-
-            item_nome.innerText = `${exercicio.nome}`
-            item_descricao.innerText = `${exercicio.descricao}`
-            modificar.innerHTML = `<button onclick="modificarExercicio(${exercicio.id})"><i class="fa-solid fa-pen-to-square"></i></button>`
-            deletar.innerHTML = `<button onclick="apagarExercicio(${exercicio.id})"><i class="fa-solid fa-trash"></i></button>`
-
-
-            linha.appendChild(item_nome)
-            linha.appendChild(item_descricao)
-            linha.appendChild(modificar)
-            linha.appendChild(deletar)
-
-            table_exercicios.appendChild(linha)
-
-
-            cx_nome.value = ''
-            cx_descricao.value = ''
-            cx_nome.focus()
-        }
+    }
+}
 
 
 
-        main()
+
+
+function adicionarItemNaLista(exercicio) {
+    const item_nome = document.createElement('td')
+    const item_descricao = document.createElement('td')
+    const modificar = document.createElement('td')
+    const deletar = document.createElement('td')
+    const linha = document.createElement('tr')
+
+    item_nome.innerText = `${exercicio.nome}`
+    item_descricao.innerText = `${exercicio.descricao}`
+    modificar.innerHTML = `<button onclick="modificarExercicio(${exercicio.id})"><i class="fa-solid fa-pen-to-square"></i></button>`
+    deletar.innerHTML = `<button onclick="apagarExercicio(${exercicio.id})"><i class="fa-solid fa-trash"></i></button>`
+
+
+    linha.appendChild(item_nome)
+    linha.appendChild(item_descricao)
+    linha.appendChild(modificar)
+    linha.appendChild(deletar)
+
+    table_exercicios.appendChild(linha)
+
+
+    cx_nome.value = ''
+    cx_descricao.value = ''
+    cx_nome.focus()
+}
+
+
+
+main()
