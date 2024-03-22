cfb6ed8f88bd6ed25bb27a2e8c0be81a93a571ad
Gisele <you@example.com>
2024-03-22 12:39:29 -0300

    função modificar criada

--- erickGym-Web/script.js
+++ erickGym-Web/script.js
@@ -69,6 +69,44 @@ async function apagarExercicio(id) {
 }
 
 
+async function modificarExercicio(id){
+    const response = await fetch(API_URL)
+    if (response.status === 200) {
+        const exercicios = await response.json()
+        for (let exercicio of exercicios) {
+            if (exercicio.id === id){
+                cx_nome.value = exercicio.nome
+                cx_descricao.value = exercicio.descricao
+
+                const nome = cx_nome.value
+                const descricao = cx_descricao.value
+
+                const dados = { nome, descricao }
+
+                const config = {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(dados)
+                }
+            
+                const response = await fetch(API_URL, config)
+            
+                if (response.status === 201) {
+                    const exercicio = await response.json()
+                    adicionarItemNaLista(exercicio)
+                    alert('Exercicio modificado com sucesso!')
+                }
+                else {
+                    alert('Erro ao modificar exercicio!')
+                }
+            }
+        }
+    }
+}
+
+
         
 function adicionarItemNaLista(exercicio) {
     const item_nome = document.createElement('td')
@@ -80,7 +118,7 @@ function adicionarItemNaLista(exercicio) {
     item_nome.innerText = `${exercicio.nome}`
     item_descricao.innerText = `${exercicio.descricao}`
     modificar.innerHTML = `<button onclick="apagarExercicio(${exercicio.id})"><i class="fa-solid fa-pen-to-square"></i></button>`
-    deletar.innerHTML = `<button ><i class="fa-solid fa-trash"></i></button>`
+    deletar.innerHTML = `<button onclick="modificarExercicio(${exercicio.id})"><i class="fa-solid fa-trash"></i></button>`
 
 
     linha.appendChild(item_nome)
