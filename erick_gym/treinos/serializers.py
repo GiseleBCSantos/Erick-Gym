c3ad13f7ee29ec5bee6bdb8da300f2d16c5d61c5
GiseleBCSantos <catce.2023111mtds0035@aluno.ifpi.edu.br>
2024-03-23 08:24:34 -0300

    adicionado id nos fields

--- erick_gym/treinos/serializers.py
+++ erick_gym/treinos/serializers.py
@@ -5,4 +5,4 @@ from .models import Exercicio
 class ExercicioSerializer(ModelSerializer):
     class Meta:
         model = Exercicio
-        fields = ['nome', 'descricao', 'em_equipamento', 'idade_minima']
+        fields = ['id', 'nome', 'descricao', 'em_equipamento', 'idade_minima']
